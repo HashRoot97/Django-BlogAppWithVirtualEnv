b23b57c8f8f48e8b7039100d198a9bedb9bc4835
HashRoot97 <roothash97@gmail.com>
2017-08-13 13:54:14 -0700

    Added functionalities

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Successfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 
 def post_detail(request, id):
@@ -26,11 +37,25 @@ def post_list(request):
     return render(request, "index.html", context)
 
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Successfully Edited")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
-
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully Deleted")
+    return redirect("posts:list")
 
